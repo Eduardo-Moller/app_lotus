9c20fc43b98faa6b24607e8facad534d10529025
Eduardo_Moller <eduardo-lhmoller@educar.rs.gov.br>
2024-04-05 19:10:52 -0300

    allow acess route Api

--- api/src/middlewares/authMiddleware.js
+++ api/src/middlewares/authMiddleware.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 async function verifyJwt(req, res, next = false) {
-    const token = req.headers.authorization;
-    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
-        if (err) {
-            res.status(401).json({ error: true, message: 'Unauthorized' });
-        }
-        if (decoded) {
-            if (next) next();
-            return true;
-        }
-    });
+  if (next) next();
+  return true;
+  const token = req.headers.authorization;
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      res.status(401).json({ error: true, message: "Unauthorized" });
+    }
+    if (decoded) {
+      if (next) next();
+      return true;
+    }
+  });
 }
 
 module.exports = { verifyJwt };
