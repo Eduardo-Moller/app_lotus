035174441252d2dcb2d46f2cbf0e8ef9f46aa788
Eduardo_Moller <eduardo-lhmoller@educar.rs.gov.br>
2024-04-07 14:26:07 -0300

    fix: create request

--- api/src/models/requestsModel.js
+++ api/src/models/requestsModel.js
@@ -1,30 +1,38 @@
-const sqlHelpers = require("../helpers/sqlHelpers");
+const sqlHelpers = require('../helpers/sqlHelpers');
 
 async function getRequests() {
-  return await sqlHelpers.selectAllTable("requests", { deleted: false });
+    return await sqlHelpers.selectAllTable('requests', { deleted: false });
 }
 
 async function getRequest(id) {
-  return await sqlHelpers.selectTable("requests", { id, deleted: false });
+    return await sqlHelpers.selectTable('requests', { id, deleted: false });
 }
 
 async function createRequest(data) {
-  var request = await sqlHelpers.insertUpdateTable("requests", data);
-  if (data.requests_materials.length > 0 && request && request.id) {
-    request.requests_materials = [];
-    data.requests_materials.forEach(async (material) => {
-      var requests_material = await sqlHelpers.insertUpdateTable(
-        "requests_materials",
-        { ...material, request_id: request.id }
-      );
-      request.requests_materials.push(requests_material);
-    });
-  }
-  return request;
+    var requests_materials = [];
+    if (data.requests_materials) {
+        requests_materials = data.requests_materials;
+        delete data.requests_materials;
+    }
+    var request = await sqlHelpers.insertUpdateTable('requests', data);
+    if (requests_materials.length > 0 && request && request.id) {
+        request.requests_materials = [];
+        requests_materials.forEach(async (material) => {
+            var requests_material = await sqlHelpers.insertUpdateTable(
+                'requests_materials',
+                { ...material, request_id: request.id }
+            );
+            request.requests_materials.push(requests_material);
+        });
+    }
+    return request;
 }
 
 async function deleteRequest(id) {
-  return await sqlHelpers.insertUpdateTable("requests", { id, deleted: true });
+    return await sqlHelpers.insertUpdateTable('requests', {
+        id,
+        deleted: true,
+    });
 }
 
 module.exports = { getRequests, getRequest, createRequest, deleteRequest };
